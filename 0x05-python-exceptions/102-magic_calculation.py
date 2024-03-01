aefe51c0e6b73db980195b7b02bcaf1d765136ec
Amos Wanyonyi <wamalabeamos@gmail>
2024-03-01 02:32:13 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,22 +4,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
-            result += a ** b / i
-        except Exception:
-            result = b + a
-            break
-    return result
-"""
-#!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
                 raise Exception('Too far')
             result += a ** b / i
         except Exception:
             result = b + a
             break
-    return result"""
+    return result
